812b6995bb5258a998d4a6d3a705ce10d4d8f83f
hassan-prog <alielsayed1112@gmail.com>
2022-12-02 22:18:36 +0200

    added appbar

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/main_drawer.dart';
+
 class FiltersScreen extends StatelessWidget {
   static const routeName = '/filters';
   const FiltersScreen({super.key});
@@ -7,6 +9,11 @@ class FiltersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Favorites'),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      // drawer: MainDrawer(),
       body: Center(
         child: Text('Filters!'),
       ),

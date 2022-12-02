548261ef6c12d449ff3c405f3f8dc1a76466cd72
hassan-prog <alielsayed1112@gmail.com>
2022-12-02 21:39:35 +0200

    added filters and tabs screen routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './screens/filters_screen.dart';
+import './screens/tabs_screen.dart';
 import './screens/meal_detail_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/catagories_screen.dart';
@@ -34,9 +36,10 @@ class MyApp extends StatelessWidget {
             ),
       ),
       routes: {
-        '/': (ctx) => CatagoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(),
       }, //routes are for the navigation of pages
       onUnknownRoute: (settings) {
         return MaterialPageRoute(builder: (ctx) => CatagoriesScreen());
